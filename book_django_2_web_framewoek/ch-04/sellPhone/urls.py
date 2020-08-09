e3eed23f0105ee4d3cb2723f9930f91e40a67e5a
Wu Justin <justin.cathay@gmail.com>
2020-08-09 21:09:26 +0800

    ch-04 範例練習

--- book_django_2_web_framewoek/ch-04/sellPhone/urls.py
+++ book_django_2_web_framewoek/ch-04/sellPhone/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mysite.views import about, listing, disp_detail
+from mysite.views import about, listing, disp_detail, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', about),
     path('list/', listing),
     path('list/<str:sku>/', disp_detail),
+    path('', index),
 ]
