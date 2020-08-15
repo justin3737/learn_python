a8a8ffc7cb7556a1aa1ace018be50e97023093a5
Justin Wu <juns3700@gmail.com>
2020-08-15 16:11:47 +0800

    rename to ch07

--- book_django_2_web_framewoek/ch07/ch07/urls.py
+++ book_django_2_web_framewoek/ch07/ch07/urls.py
@@ -1,4 +1,4 @@
-"""ch05 URL Configuration
+"""ch07 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
--- book_django_2_web_framewoek/ch07/ch07/wsgi.py
+++ book_django_2_web_framewoek/ch07/ch07/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ch05 project.
+WSGI config for ch07 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ch05.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ch07.settings")
 
 application = get_wsgi_application()
