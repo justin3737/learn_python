dfea840937a474778b9975f24b7277db028fcd63
Justin Wu <juns3700@gmail.com>
2020-08-15 17:36:12 +0800

    建立models and db

--- book_django_2_web_framewoek/ch07/mysite/admin.py
+++ book_django_2_web_framewoek/ch07/mysite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from mysite import models
 
 # Register your models here.
+admin.site.register(models.Maker)
+admin.site.register(models.PModel)
+admin.site.register(models.Product)
+admin.site.register(models.PPhoto)
--- book_django_2_web_framewoek/ch07/mysite/models.py
+++ book_django_2_web_framewoek/ch07/mysite/models.py
@@ -1,3 +1,40 @@
+# -*- encoding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
+
+class Maker(models.Model):
+    name = models.CharField(max_length=10)
+    country = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class PModel(models.Model):
+    maker = models.ForeignKey(Maker, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    url = models.URLField(default='https://imgur.com/TQTfGiA')  #說明此規格的網址
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    pmodel = models.ForeignKey(PModel, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=15, default='超值Xpro3')
+    description = models.TextField(default='暫無說明')
+    year = models.PositiveIntegerField(default=2020)
+    price = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.nickname
+
+
+class PPhoto(models.Model):
+    pmodel = models.ForeignKey(Product, on_delete=models.CASCADE)
+    description = models.CharField(max_length=20, default='產品照片')
+    url = models.URLField(default='https://imgur.com/TQTfGiA')  #儲存照片的位置
+
+    def __str__(self):
+        return self.description
