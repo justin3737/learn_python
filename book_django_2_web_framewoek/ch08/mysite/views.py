85cecebac860dd8df2b697a1f5e8ba9af51520f2
Justin Wu <juns3700@gmail.com>
2020-08-23 17:37:19 +0800

    add ch08 example

--- book_django_2_web_framewoek/ch08/mysite/views.py
+++ book_django_2_web_framewoek/ch08/mysite/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    years = range(1960,2021)
+    try:
+        urid = request.GET['user_id']
+        urpass = request.GET['user_pass']
+        uryear = request.GET['byear']
+        urfcolor = request.GET.getlist('fcolor')
+    except:
+        urid = None
+    
+    if urid != None and urpass == '12345':
+        verified = True 
+    else:
+        verified = False
+    return render(request, 'index.html', locals())
